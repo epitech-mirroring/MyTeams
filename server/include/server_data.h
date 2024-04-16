c91bc500182b41dd127635955851e26687eecaf1
RenardFute <axel.eckenberg@yahoo.fr>
2024-04-16 23:31:05 +0200

    :memo: Add documentation

--- server/include/server_data.h
+++ server/include/server_data.h
@@ -10,39 +10,185 @@
 #include "server_types.h"
 #include "json/json.h"
 
-#define COPY_UUID(dest, src) memcpy(dest, src, 16)
-
+/**
+ * @brief Save the server data to a file
+ * @param server The server to save
+ */
 void save_data(roundtable_server_t *server);
+/**
+ * @brief Serialize the server data to a json object
+ * @param server The server to serialize
+ * @return The serialized server data
+ */
 json_array_t *serialize_clients(roundtable_server_t *server);
+/**
+ * @brief Serialize a client to a json object
+ * @param client The client to serialize
+ * @return The serialized client data
+ */
 json_object_t *serialize_client(roundtable_client_t *client);
+/**
+ * @brief Serialize the teams of a server to a json array
+ * @param server The server to serialize
+ * @return The serialized teams
+ */
 json_array_t *serialize_teams(roundtable_server_t *server);
+/**
+ * @brief Serialize a team to a json object
+ * @param team The team to serialize
+ * @return The serialized team data
+ */
 json_object_t *serialize_team(roundtable_team_t *team);
+/**
+ * @brief Serialize the channels of a server to a json array
+ * @param server The server to serialize
+ * @return The serialized channels
+ */
 json_array_t *serialize_channels(roundtable_server_t *server);
+/**
+ * @brief Serialize a channel to a json object
+ * @param channel The channel to serialize
+ * @return The serialized channel data
+ */
 json_object_t *serialize_channel(roundtable_channel_t *channel);
+/**
+ * @brief Serialize the threads of a server to a json array
+ * @param server The server to serialize
+ * @return The serialized threads
+ */
 json_array_t *serialize_threads(roundtable_server_t *server);
+/**
+ * @brief Serialize a thread to a json object
+ * @param thread The thread to serialize
+ * @return The serialized thread data
+ */
 json_object_t *serialize_thread(roundtable_thread_t *thread);
+/**
+ * @brief Serialize the messages of a server to a json array
+ * @param server The server to serialize
+ * @return The serialized messages
+ */
 json_array_t *serialize_messages(roundtable_server_t *server);
+/**
+ * @brief Serialize a message to a json object
+ * @param message The message to serialize
+ * @return The serialized message data
+ */
 json_object_t *serialize_message(roundtable_message_t *message);
+/**
+ * @brief Serialize the subscribers of a server to a json array
+ * @param server The server to serialize
+ * @return The serialized subscribers
+ */
 json_array_t *serialize_subscribers(roundtable_server_t *server);
+/**
+ * @brief Serialize the direct messages of a server to a json array
+ * @param server The server to serialize
+ * @return The serialized direct messages
+ */
 json_array_t *serialize_direct_messages(roundtable_server_t *server);
+// --------------------------- LOAD ------------------------------
+/**
+ * @brief Serialize a direct message to a json object
+ * @param direct_message The direct message to serialize
+ * @return The serialized direct message data
+ */
 void load_data(roundtable_server_t *server, const char *path);
+/**
+ * @brief Deserialize the clients of a server
+ * @param server The server to deserialize the clients to (will be filled
+ * with the clients)
+ * @param clients The clients to deserialize
+ */
 void deserialize_clients(roundtable_server_t *server, json_array_t *clients);
+/**
+ * @brief Deserialize a client
+ * @param server The server to deserialize the client to
+ * @param client The client to deserialize
+ */
 void deserialize_client(roundtable_server_t *server, json_object_t *client);
+/**
+ * @brief Deserialize the teams of a server
+ * @param server The server to deserialize the teams to (will be filled
+ * with the teams)
+ * @param teams The teams to deserialize
+ */
 void deserialize_teams(roundtable_server_t *server, json_array_t *teams);
+/**
+ * @brief Deserialize a team
+ * @param server The server to deserialize the team to
+ * @param team The team to deserialize
+ */
 void deserialize_team(roundtable_server_t *server, json_object_t *team);
+/**
+ * @brief Deserialize the channels of a team
+ * @param channels The channels to deserialize
+ * @param team The team to add the channels to
+ */
 void deserialize_channels(json_array_t *channels, roundtable_team_t *team);
+/**
+ * @brief Deserialize a channel
+ * @param channel The channel to deserialize
+ * @param team The team to add the channel to
+ */
 void deserialize_channel(json_object_t *channel, roundtable_team_t *team);
+/**
+ * @brief Deserialize the threads of a channel
+ * @param threads The threads to deserialize
+ * @param channel The channel to add the threads to
+ */
 void deserialize_threads(json_array_t *threads, roundtable_channel_t *channel);
+/**
+ * @brief Deserialize a thread
+ * @param thread The thread to deserialize
+ * @param channel The channel to add the thread to
+ */
 void deserialize_thread(json_object_t *thread, roundtable_channel_t *channel);
+/**
+ * @brief Deserialize the messages of a thread
+ * @param messages The messages to deserialize
+ * @param thread The thread to add the messages to
+ */
 void deserialize_messages(json_array_t *messages, roundtable_thread_t *thread);
+/**
+ * @brief Deserialize a message
+ * @param message The message to deserialize
+ * @param thread The thread to add the message to
+ */
 void deserialize_message(json_object_t *message, roundtable_thread_t *thread);
+/**
+ * @brief Deserialize the subscribers of a team
+ * @param server The server to search the clients in
+ * @param subscribers The subscribers to deserialize
+ * @param team The team to add the subscribers to
+ */
 void deserialize_subscribers(roundtable_server_t *server,
     json_array_t *subscribers, roundtable_team_t *team);
+/**
+ * @brief Deserialize the messages of a direct message
+ * @param server The server to add the direct message to
+ * @param direct_messages The direct messages to deserialize
+ */
 void deserialize_direct_messages(roundtable_server_t *server,
     json_array_t *direct_messages);
+/**
+ * @brief Deserialize a direct message
+ * @param server
+ * @param message
+ */
 void deserialize_direct_message(roundtable_server_t *server,
     json_object_t *message);
+/**
+ * @brief Deserialize the messages of a direct message
+ * @param messages the messages to deserialize
+ * @param direct_message the direct message to add the messages to
+ */
 void deserialize_direct_message_messages(json_array_t *messages,
     roundtable_direct_message_t *direct_message);
+/**
+ * @brief Deserialize a direct message message
+ * @param message the message to deserialize
+ * @param direct_message the direct message to add the message to
+ */
 void deserialize_direct_message_message(json_object_t *message,
     roundtable_direct_message_t *direct_message);
--- server/include/server_types.h
+++ server/include/server_types.h
@@ -7,15 +7,27 @@
 */
 
 #pragma once
-
 #include <stddef.h>
+#include <string.h>
 #include "uuid/uuid.h"
 
+/**
+ * @brief Copy a UUID from src to dest
+ * @note This is a macro because the uuid_t type is an array of 16 bytes
+ */
+#define COPY_UUID(dest, src) memcpy(dest, src, 16)
+
+/**
+ * @brief A message sent by a client in a thread or a direct message
+ */
 typedef struct roundtable_message_s {
     uuid_t sender_uuid;
     char *content;
 } roundtable_message_t;
 
+/**
+ * @brief A thread in a channel
+ */
 typedef struct roundtable_thread_s {
     uuid_t uuid;
     char *title;
@@ -24,6 +36,10 @@ typedef struct roundtable_thread_s {
     size_t message_count;
 } roundtable_thread_t;
 
+/**
+ * @brief A channel in a team
+ * @note A channel can have multiple threads
+ */
 typedef struct roundtable_channel_s {
     uuid_t uuid;
     char *name;
@@ -32,6 +48,9 @@ typedef struct roundtable_channel_s {
     size_t thread_count;
 } roundtable_channel_t;
 
+/**
+ * @brief A direct message between two clients
+ */
 typedef struct roundtable_direct_message_s {
     uuid_t sender_uuid;
     uuid_t receiver_uuid;
@@ -39,6 +58,9 @@ typedef struct roundtable_direct_message_s {
     size_t message_count;
 } roundtable_direct_message_t;
 
+/**
+ * @brief The status of a client
+ */
 typedef enum roundtable_status_s {
     ONLINE,
     OFFLINE,
@@ -46,12 +68,19 @@ typedef enum roundtable_status_s {
     BUSY
 } roundtable_status_t;
 
+/**
+ * @brief A client registered in the server
+ */
 typedef struct roundtable_client_s {
     uuid_t uuid;
     char *username;
     roundtable_status_t status;
 } roundtable_client_t;
 
+/**
+ * @brief A team in the server
+ * @note A team can have multiple channels and subscribers
+ */
 typedef struct roundtable_team_s {
     uuid_t uuid;
     char *name;
@@ -62,6 +91,9 @@ typedef struct roundtable_team_s {
     size_t subscriber_count;
 } roundtable_team_t;
 
+/**
+ * @brief The server containing all the data
+ */
 typedef struct roundtable_server_s {
     roundtable_team_t *teams;
     size_t team_count;
