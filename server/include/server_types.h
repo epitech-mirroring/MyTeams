5ec52f9ddbea93f98a300564ba2e6eca7475678f
RenardFute <axel.eckenberg@yahoo.fr>
2024-04-16 22:21:51 +0200

    :sparkles: Add missing counts for arrays

--- server/include/server_types.h
+++ server/include/server_types.h
@@ -7,6 +7,8 @@
 */
 
 #pragma once
+
+#include <stddef.h>
 #include "uuid/uuid.h"
 
 typedef struct roundtable_message_s {
@@ -19,6 +21,7 @@ typedef struct roundtable_thread_s {
     char *title;
     char *content;
     roundtable_message_t *messages;
+    size_t message_count;
 } roundtable_thread_t;
 
 typedef struct roundtable_channel_s {
@@ -26,12 +29,14 @@ typedef struct roundtable_channel_s {
     char *name;
     char *description;
     roundtable_thread_t *threads;
+    size_t thread_count;
 } roundtable_channel_t;
 
 typedef struct roundtable_direct_message_s {
     uuid_t sender_uuid;
     uuid_t receiver_uuid;
     roundtable_message_t *messages;
+    size_t message_count;
 } roundtable_direct_message_t;
 
 typedef enum roundtable_status_s {
@@ -52,11 +57,16 @@ typedef struct roundtable_team_s {
     char *name;
     char *description;
     roundtable_channel_t *channels;
+    size_t channel_count;
     roundtable_client_t *subscribers;
+    size_t subscriber_count;
 } roundtable_team_t;
 
 typedef struct roundtable_server_s {
     roundtable_team_t *teams;
+    size_t team_count;
     roundtable_client_t *clients;
+    size_t client_count;
     roundtable_direct_message_t *direct_messages;
+    size_t message_count;
 } roundtable_server_t;
