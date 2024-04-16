66d0588fbaa16fd7345c719420a6990bdaf1deab
RenardFute <axel.eckenberg@yahoo.fr>
2024-04-16 23:21:54 +0200

    :construction: Working on main entrypoint

--- server/src/main.c
+++ server/src/main.c
@@ -6,9 +6,18 @@
 ** You can even have multiple lines if you want !
 */
 
+#include "server.h"
+#include "server_data.h"
+
 int main(int ac, char **av)
 {
-    (void)ac;
-    (void)av;
+    roundtable_server_t *server = create_server();
+
+    (void) ac;
+    (void) av;
+    if (!server)
+        return 84;
+    load_data(server, "./data.json");
+    destroy_server(server);
     return 0;
 }
