45b096ef352a5cd8037dfdbb12f1d5bfef8f3591
Marius-Pn1 <marius.pain@epitech.eu>
2024-04-19 01:46:53 +0200

    :sparkles: Update login command to print login success message

--- client/src/commands/login.c
+++ client/src/commands/login.c
@@ -19,9 +19,8 @@ void login_resp(response_t *response, request_data_t *request_data)
     if (response->status == 200 ||
         response->status == 201) {
         jobj = (json_object_t *)json_parse(response->body);
-        if (jobj == NULL) {
+        if (jobj == NULL)
             return;
-        }
         str = (json_string_t *)json_object_get(jobj, "user_uuid");
         if (str == NULL) {
             printf("Error: invalid response\n");
@@ -29,7 +28,8 @@ void login_resp(response_t *response, request_data_t *request_data)
         }
         cli->user_uuid = strdup(str->value);
         cli->is_logged = true;
-        client_event_logged_in(cli->user_uuid, cli->user_name);
+        printf("CLI : Successfully logged in as %s with uuid %s\n",
+            cli->user_name, cli->user_uuid);
     }
 }
 
@@ -45,7 +45,8 @@ static void send_login(client_t *client, char *user_name)
     json_object_add(jobj, (json_t *)username);
     request->route = (route_t){"POST", "/login"};
     request->body = json_serialize((json_t *)jobj);
-    api_client_send_request(client->api_handler, request, &login_resp, client);
+    api_client_send_request(client->api_handler, request, &login_resp,
+        client);
     client->waiting_for_response = true;
     client->user_name = strdup(user_name);
 }
@@ -65,7 +66,8 @@ void login(char **parsed_cmd, client_t *client)
     }
     name_len = strlen(parsed_cmd[1]);
     if (name_len < 1 || name_len > MAX_NAME_LENGTH) {
-        printf("Error: username must be between 1 and %i characters\n", MAX_NAME_LENGTH);
+        printf("Error: username must be between 1 and %i characters\n",
+            MAX_NAME_LENGTH);
         return;
     }
     send_login(client, parsed_cmd[1]);
