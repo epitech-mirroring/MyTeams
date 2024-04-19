8650cee9bccf4d67a59d74c38fbd8be90c373301
RenardFute <axel.eckenberg@yahoo.fr>
2024-04-19 14:00:12 +0200

    :typo: Fix param name

--- server/src/routes/teams/users.c
+++ server/src/routes/teams/users.c
@@ -45,13 +45,13 @@ response_t team_users_route(request_t *request, void *data)
         return create_error(405, "Method not allowed", "Only GET");
     if (!request_has_header(request, "Authorization"))
         return create_error(401, "Unauthorized", "Missing 'Authorization'");
-    if (!request_has_param(request, "team_uuid"))
-        return create_error(400, "Invalid body", "Missing 'team_uuid'");
+    if (!request_has_param(request, "team-uuid"))
+        return create_error(400, "Invalid params", "Missing 'team-uuid'");
     client = get_client_from_header(server, request);
     if (!client)
         return create_error(401, "Unauthorized", "Invalid 'Authorization'");
     team = get_team_from_string(server,
-        request_get_param(request, "team_uuid"));
+        request_get_param(request, "team-uuid"));
     if (!team)
         return create_error(404, "Team not found", "Team not found");
     if (!roundtable_team_has_subscriber(team, client))
