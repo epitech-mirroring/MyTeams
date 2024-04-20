ebeb7508b9f652b04b2173d182b35a9a908866a1
Marius-Pn1 <marius.pain@epitech.eu>
2024-04-20 23:59:12 +0200

    :sparkles: Add info_channel command to client code

--- client/include/myclient.h
+++ client/include/myclient.h
@@ -97,6 +97,7 @@ void create_reply(char **parsed_cmd, client_t *client);
 void info(char **parsed_cmd, client_t *client);
 void info_user(client_t *client);
 void info_team(client_t *client);
+void info_channel(client_t *client);
 
 void send_events(client_t *client);
 void logged_in_callback(json_object_t *data, client_t *client);
