8b8525006e4c2e8129b6a4b4a1ff8bdacf9a2eb4
RenardFute <axel.eckenberg@yahoo.fr>
2024-04-20 18:18:15 +0200

    :bug: Fix team creation event only send to subscribers

--- server/src/events/event_team_created.c
+++ server/src/events/event_team_created.c
@@ -14,9 +14,9 @@ void roundtable_event_team_created(roundtable_server_t *server,
     json_object_t *data = NULL;
     roundtable_client_t *client = NULL;
 
-    for (size_t i = 0; i < team->subscriber_count; i++) {
+    for (size_t i = 0; i < server->client_count; i++) {
         client = roundtable_server_get_client_by_uuid(server,
-            team->subscribers[i]);
+            server->clients[i]->uuid);
         data = json_object_create(NULL);
         json_object_add(data, (json_t *) json_string_create("team_uuid",
             uuid_to_string(team->uuid)));
