a1cc18f1ca5ca494f809b111994b4602dfbec929
Marius-Pn1 <marius.pain@epitech.eu>
2024-04-20 19:18:16 +0200

    :sparkles: Add logged_in_callback function to handle logged in events

--- client/include/myclient.h
+++ client/include/myclient.h
@@ -95,3 +95,4 @@ void create_thread(char **parsed_cmd, client_t *client);
 void create_reply(char **parsed_cmd, client_t *client);
 
 void send_events(client_t *client);
+void logged_in_callback(json_object_t *data, client_t *client);
