f7825167e20924647d57b2400314f5baf1e6ecf4
6im0n <simon.gl3135@orange.fr>
2024-04-20 04:02:43 +0200

    :adhesive_bandage: Fix case of error in when get list of thread

--- server/src/threads/utils.c
+++ server/src/threads/utils.c
@@ -8,6 +8,7 @@
 
 #include <stdlib.h>
 #include "server.h"
+#include "network/dto.h"
 
 void roundtable_thread_add_message(roundtable_thread_t *thread,
     roundtable_message_t *message)
@@ -21,4 +22,55 @@ void roundtable_thread_add_message(roundtable_thread_t *thread,
     thread->messages = new_messages;
     thread->messages[thread->message_count] = *message;
     thread->message_count++;
+}
+
+roundtable_thread_t *get_thread_from_param(roundtable_channel_t *channel,
+    request_t *request, roundtable_server_t *server, char *key)
+{
+    uuid_t *thread_uuid = uuid_from_string(request_get_param(request, key));
+    roundtable_thread_t *thread = NULL;
+
+    if (thread_uuid == NULL)
+        return NULL;
+    for (size_t i = 0; i < channel->thread_count; i++) {
+        if (uuid_compare(channel->threads[i].uuid, *thread_uuid)) {
+            thread = &channel->threads[i];
+            break;
+        }
+    }
+    return thread;
+}
+
+roundtable_channel_t *get_channel_from_param(roundtable_team_t *team,
+    request_t *request, roundtable_server_t *server, char *key)
+{
+    uuid_t *channel_uuid = uuid_from_string(request_get_param(request, key));
+    roundtable_channel_t *channel = NULL;
+
+    if (channel_uuid == NULL)
+        return NULL;
+    for (size_t i = 0; i < team->channel_count; i++) {
+        if (uuid_compare(team->channels[i].uuid, *channel_uuid)) {
+            channel = &team->channels[i];
+            break;
+        }
+    }
+    return channel;
+}
+
+roundtable_team_t *get_team_from_param(request_t *request,
+    roundtable_server_t *server, char *key)
+{
+    uuid_t *team_uuid = uuid_from_string(request_get_param(request, key));
+    roundtable_team_t *team = NULL;
+
+    if (team_uuid == NULL)
+        return NULL;
+    for (size_t i = 0; i < server->team_count; i++) {
+        if (uuid_compare(server->teams[i]->uuid, *team_uuid)) {
+            team = server->teams[i];
+            break;
+        }
+    }
+    return team;
 }
